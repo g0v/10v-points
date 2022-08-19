902113dcdb38a993cabcfd742318668f8b866c67
ddio <ddioibb@gmail.com>
2022-08-19 14:56:55 +0800

    feat: add partner page routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,63 @@
 <template lang="pug">
-    .index.mw7.mv4.ph3.center
-        h1.f1.fw6.tc ASSEMBLE!專案者聯盟！
-        .mv4.mw6.lh-copy.center
-            p 來派對除了吃吃喝喝，當然是要玩遊戲的吧！g0v十週年也不例外，在今年這個十週年的盛會，我們也跟g0v社群中不同專案的坑主合作，讓來參加派對的參與者除了吃喝玩樂，也可以參與集點互動式闖關遊戲，除了更加認識不同的專案，也能貢獻自己的小小力量，一起召喚十週年多重宇宙的專案者聯盟！
-            p 我們在十週年生日趴的場地中設置了跟不同專案有關的闖關QR code，參與者可以在會場中尋找這些QR code 進入各個闖關活動，完成任務後就可以獲得獎勵！\
-        .index__partnerList
-            +each('partnerList as partner')
-                .index__partner
-                    h2.mt0.mb2.f4 {partner.title}
-                    .aspect-ratio.aspect-ratio--1x1.mb3
-                        img.aspect-ratio--object(src="{partner.coverImg}")
-                    +each('partner.description as line')
-                        p.lh-copy.mb2.mt0 {line}
+  .index.mw7.mv4.ph3.center
+    h1.f1.fw7.tc {title}
+    .mv4.mw6.lh-copy.center
+      p
+        | 來派對除了吃吃喝喝，當然是要玩遊戲的吧！g0v十週年也不例外，在今年這個十週年的盛會，
+        | 我們也跟g0v社群中不同專案的坑主合作，讓來參加派對的參與者除了吃喝玩樂，也可以參與集
+        | 點互動式闖關遊戲，除了更加認識不同的專案，也能貢獻自己的小小力量，一起召喚十週年多重
+        | 宇宙的專案者聯盟！
+      p
+        | 我們在十週年生日趴的場地中設置了跟不同專案有關的闖關QR code，參與者可以在會場中尋
+        | 找這些QR code 進入各個闖關活動，完成任務後就可以獲得獎勵！
+    .index__partnerList
+      +each('partnerList as partner')
+        a.aspect-ratio.aspect-ratio--1x1.pointer(href="{partner.link}")
+          .aspect-ratio--object.no-repeat.cover(
+            style="background-image: url('{partner.coverImg}')"
+          )
+            .index__partner.flex.flex-column.justify-end.pa3.white.h-100.tr
+              h2.mv0.f4.fw7.flex.items-center.justify-end
+                i.fa-solid.fa-hand-spock.f5.mr2
+                | {partner.title}
+              .mt3.f6
+                +each('partner.description as line')
+                  p.lh-copy.mb1.mt0 {line}
 
-            
+      
 </template>
 <script>
-    import 'tachyons/css/tachyons.css'
-    import partnerRawList from '$lib/assets/partners.json';
+  import 'tachyons/css/tachyons.css'
+  import partnerRawList from '$lib/assets/partners.json';
 
-    const partnerList = partnerRawList.map((partner) => {
-        return {
-            ...partner,
-            description: partner.description.split('\n')
-        }
-    })
+  const title = 'ASSEMBLE!專案者聯盟！'
+
+  const partnerList = partnerRawList.map((partner) => {
+    return {
+      ...partner,
+      description: partner.description.split('\n'),
+      link: `/partner/${partner.title}`
+    }
+  })
 </script>
 <svelte:head>
-  <title>Welcome</title>
+  <title>{title}</title>
 </svelte:head>
 <style lang="scss">
 .index {
-    &__partnerList {
-        display: grid;
-        row-gap: 1rem;
+  &__partnerList {
+    display: grid;
+    row-gap: 1rem;
 
-        @media screen and (min-width: 30em) {
-          display: grid;
-          grid-template-columns: 1fr 1fr;
-          column-gap: 1rem;
-        }
+    @media screen and (min-width: 30em) {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      column-gap: 1rem;
     }
-    &__partner {
-        img {
-            object-fit: cover;
-        }
-    }
+  }
+  &__partner {
+    background: linear-gradient(180deg, #fff0 20%, #222a 60%); 
+    text-shadow: 0 0 5px black;
+  }
 }
 </style>
--- src/routes/partner/[partner]/+page.svelte
+++ src/routes/partner/[partner]/+page.svelte
@@ -1,11 +1,18 @@
 <script>
-    import { page } from '$app/stores';
-    import partnerMap from '$lib/assets/partners.json';
-    console.log($page);
+  import { page } from '$app/stores';
+  import partnerMap from '$lib/assets/partners.json';
+  const partner = partnerMap.find(partner => partner.title === $page.params.partner)
+  const pageTitle = partner?.title
 </script>
 
 <svelte:head>
-  <title>Welcome {$page.params.partner}!</title>
+  <title>{pageTitle} x ASSEMBLE!專案者聯盟！</title>
 </svelte:head>
-<h1>Hi {$page.params.partner}!</h1>
-<pre>{JSON.stringify(partnerMap, null, '  ')}</pre>
+<template lang="pug">
+  .index.mw7.mv4.ph3.center
+    +if('!partner')
+      h1.mv5 找不到「{$page.params.partner}」呦 🙄🙄🙄 ，是誰帶你來這裡的？
+      +else()
+        h1 嗨！ {pageTitle}
+        .gray.lh-copy {JSON.stringify(partner)}
+</template>
