da7a2989f1e66e313adfee58aa6939cbd1ed0f0f
ddio <ddio@users.noreply.github.com>
2022-08-19 22:20:47 +0000

    deploy: 94429339f41b19aaaf0c35b4bce9b103ad771aad

---
+++ _app/immutable/chunks/6-cc6ee24f.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/partner/_partner_/_page.svelte-9d04e0f9.js";import"./index-1a2b90b6.js";import"./stores-6cc6c2f0.js";import"./utils-391638c9.js";export{e as component};
