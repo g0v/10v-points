e5f37350d5bdfa27d367db838f3c3d3d004302a2
ddio <ddio@users.noreply.github.com>
2022-08-27 21:09:03 +0000

    deploy: 446199e8252d200a6ef32e3f8bfa0287e7bf2ab3

---
+++ _app/immutable/chunks/2-0854c082.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_page.svelte-65db95d9.js";import"./index-1a2b90b6.js";import"./utils-59df27d2.js";export{r as component};
--- _app/immutable/chunks/6-95ccd17c.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/pages/partner/_partner_/_page.svelte-e29d3ad5.js";import"./index-1a2b90b6.js";import"./stores-6cc6c2f0.js";import"./utils-3066f1ad.js";export{e as component};
